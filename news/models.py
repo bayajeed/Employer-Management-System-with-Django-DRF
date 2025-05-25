0ed4e549436034526a584473313f3e7bd95c50ad
bayajeed <bayajeedhossain@gmail.com>
2025-05-26 02:54:42 +0600

    Modify News pages

--- news/models.py
+++ news/models.py
@@ -7,6 +7,7 @@ class NewsPost(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add = True)
     author = models.CharField(max_length=100)
+    image = models.ImageField(upload_to= 'news/', blank=True, null=True)
 
     def __str__(self):
         return self.title
--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import news
 
+# for media file
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('news', news, name='news')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
