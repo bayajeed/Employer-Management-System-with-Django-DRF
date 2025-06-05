59cd2c91f42e9e175345669b3cd2910fdf985f9a
bayajeed <bayajeedhossain@gmail.com>
2025-06-06 03:32:29 +0600

    DB MySQL connect with Django setting

--- config/settings.py
+++ config/settings.py
@@ -84,11 +84,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ems', # Name of your database
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Bayajeed#312',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- news/models.py
+++ news/models.py
@@ -9,5 +9,6 @@ class NewsPost(models.Model):
     author = models.CharField(max_length=100)
     image = models.ImageField(upload_to= 'news/', blank=True, null=True)
 
-    def __str__(self):
+    def __str__(self): 
         return self.title
+    
